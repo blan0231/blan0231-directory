b91368bbe139bde4aa1fa674faaf2bae402243ca
Aiden Blancher <blan0231@algonquinlive.com>
2025-02-04 09:29:48 -0500

    Completes the Assignment

--- functions.js
+++ functions.js
@@ -1,4 +1,3 @@
-// 1. List function using map()
 function list(clients) {
     return clients
       .map(
@@ -10,22 +9,18 @@ function list(clients) {
       .join("");
   }
   
-  // 2. Order function using sort()
   function order(clients, property) {
     return [...clients].sort((a, b) => (a[property] > b[property] ? 1 : -1));
   }
   
-  // 3. Total function using reduce()
   function total(clients) {
     return clients.reduce((sum, client) => sum + client.balance, 0);
   }
   
-  // 4. Info function using find()
   function info(index) {
     return clients.find((client) => client.index === index);
   }
   
-  // 5. Search function using filter()
   function search(query) {
     return clients.filter((client) =>
       client.name.toLowerCase().includes(query.toLowerCase())
